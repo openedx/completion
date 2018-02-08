bcaa83173dd6e074af3caba5a4ea6ab0ff02ba64
Alex Dusenbery <adusenbery@edx.org>
2018-02-08 10:36:01 -0500

    Add auto-generated get_latest_by migration file for BlockCompletion.

--- completion/__init__.py
+++ completion/__init__.py
@@ -5,6 +5,6 @@ Completion App
 from __future__ import unicode_literals
 
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 default_app_config = 'completion.apps.CompletionAppConfig'  # pylint: disable=invalid-name
