e97a5124b400382a12bc272641328c08aa05e45d
sandroroux <aroux@edx.org>
2018-02-15 13:28:13 -0500

    This completion work enables resume buttons on the dashboard.
    get_url_to_last_completed_block became get_key_to_last_completed_block. Changing the method spec jettisoned dependencies on edx-platform.
    
    Unit tests for completion/utilities.py.
    
    I created a public utility for submitting completion data from tests.
    
    Response to review comments.
    
    Add exceptions.py to PR.
    
    Changes hide dashboard resume buttons behind the visual progress waffle flag. get_key_to_last_completed_block checks for all exceptions thrown by BlockCompletion.

--- completion/__init__.py
+++ completion/__init__.py
@@ -5,6 +5,6 @@ Completion App
 from __future__ import unicode_literals
 
 
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 
 default_app_config = 'completion.apps.CompletionAppConfig'  # pylint: disable=invalid-name
---
+++ completion/tests/test_utilities.py
@@ -0,0 +1,51 @@
+'''
+The unit tests for utilities.py.
+'''
+from __future__ import unicode_literals
+from mock import patch
+
+from django.test import TestCase
+from opaque_keys.edx.keys import CourseKey
+from six import text_type
+
+from ..exceptions import UnavailableCompletionData
+from ..utilities import get_key_to_last_completed_course_block
+from ..test_utils import UserFactory, CompletionSetUpMixin, submit_completions_for_testing
+
+
+class TestCompletionUtilities(CompletionSetUpMixin, TestCase):
+    '''
+    Tests methods in completion's external-facing API.
+    '''
+
+    COMPLETION_SWITCH_ENABLED = True
+
+    def setUp(self):
+        super(TestCompletionUtilities, self).setUp()
+        self.user = UserFactory.create()
+        self.course_key = CourseKey.from_string("course-v1:edX+MOOC101+2049_T2")
+        self.block_keys = [
+            self.course_key.make_usage_key('video', text_type(number))
+            for number in range(5)
+        ]
+        submit_completions_for_testing(self.user, self.course_key, self.block_keys)
+
+    def test_can_get_key_to_last_completed_course_block(self):
+        with patch('completion.utilities.visual_progress_enabled', return_value=True):
+            last_block_key = get_key_to_last_completed_course_block(
+                self.user,
+                self.course_key
+            )
+
+        expected_block_usage_key = self.course_key.make_usage_key(
+            "video",
+            text_type(4)
+        )
+        self.assertEqual(last_block_key, expected_block_usage_key)
+
+    def test_getting_last_completed_course_block_in_untouched_enrollment_throws(self):
+        course_key = CourseKey.from_string("edX/NotACourse/2049_T2")
+
+        with patch('completion.utilities.visual_progress_enabled', return_value=True):
+            with self.assertRaises(UnavailableCompletionData):
+                get_key_to_last_completed_course_block(self.user, course_key)
