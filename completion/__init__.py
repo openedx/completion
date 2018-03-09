da4f582c028ec803e8dd958b869bdbfa41a296b7
J. Cliff Dyer <jcd@sdf.org>
2018-03-09 15:00:24 -0500

    Fix Django 2.0 incompatibility.

--- completion/__init__.py
+++ completion/__init__.py
@@ -5,6 +5,6 @@ Completion App
 from __future__ import unicode_literals
 
 
-__version__ = '0.0.9'
+__version__ = '0.0.10'
 
 default_app_config = 'completion.apps.CompletionAppConfig'  # pylint: disable=invalid-name
