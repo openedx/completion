5cffa10e4aa4dbc9451fec4273a81855728f32b6
John Eskew <jeskew@edx.org>
2018-09-26 09:37:25 -0400

    Fix Django20 deprecation warning.

--- completion/api/urls.py
+++ completion/api/urls.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import include, url
 
+app_name = 'completion'  # pylint: disable=invalid-name
 urlpatterns = [
-    url(r'^v1/', include('completion.api.v1.urls', namespace='v1')),
+    url(r'^v1/', include('completion.api.v1.urls')),
 ]
