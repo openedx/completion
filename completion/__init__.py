5ffbc3a23bd1c128192b66a97f7db05a67e73a98
Aarif <mraarif@outlook.com>
2020-04-21 18:10:01 +0500

    updated the tox and travis configs

--- completion/__init__.py
+++ completion/__init__.py
@@ -5,6 +5,6 @@ Completion App
 from __future__ import unicode_literals
 
 
-__version__ = '3.1.1'
+__version__ = '3.2.0'
 
 default_app_config = 'completion.apps.CompletionAppConfig'  # pylint: disable=invalid-name
