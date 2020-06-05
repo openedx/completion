435b5f61f977f4fad97e4a1dc3385226cb00eaea
Ben Warzeski <bwarzeski@edx.org>
2020-06-05 14:34:30 -0400

    Update __init__.py (#92)

--- completion/__init__.py
+++ completion/__init__.py
@@ -5,6 +5,6 @@ Completion App
 from __future__ import unicode_literals
 
 
-__version__ = '3.2.0'
+__version__ = '3.2.1'
 
 default_app_config = 'completion.apps.CompletionAppConfig'  # pylint: disable=invalid-name
